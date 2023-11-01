a9744e8093e9c572107505a8ca6e0fb3504e9b72
Pedro Rodrigues <ukpedropt@hotmail.com>
2023-11-01 21:37:18 +0000

    Implement backend file upload functionality

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template_string
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'  # specify your upload folder
 
 @app.route('/')
 def home():
     return "Hello, Loadify!"
+
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'jpg', 'pdf', 'docx', 'ppt'}
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload'))
+    return render_template_string(open("templates/upload.html").read())
 
 if __name__ == '__main__':
     app.run(debug=True)
