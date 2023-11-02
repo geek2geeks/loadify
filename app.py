c519f37d608b4fd147b0d07e54ccbcf97acee79d
Pedro Rodrigues <ukpedropt@hotmail.com>
2023-11-02 21:45:40 +0000

    Implemented user authentication and concurrent user handling

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-from flask import Flask, request, send_from_directory, render_template, jsonify  # Import jsonify
+from flask import Flask, request, send_from_directory, render_template, jsonify, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 import os
 import subprocess
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'  # Change this to a random secret key
 app.config['UPLOAD_FOLDER'] = 'static/uploads'  # specify your upload folder
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_id = request.form['user_id']
+        user = User(user_id)
+        login_user(user)
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
 @app.route('/')
+@login_required
 def home():
     return render_template("upload.html")  # Serve the upload.html template on the home page
 
 def allowed_file(filename):
-    ALLOWED_EXTENSIONS = {'jpg', 'pdf', 'docx', 'ppt'}
+    ALLOWED_EXTENSIONS = {'jpg', 'png', 'pdf', 'docx', 'ppt'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload', methods=['POST'])
+@login_required
 def upload():
     file = request.files['file']
     if file and allowed_file(file.filename):
@@ -30,6 +60,7 @@ def upload():
         return jsonify({'filename': filename})  # Return the filename as a JSON response
 
 @app.route('/uploads/<filename>')
+@login_required
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
