8f9ae0d629c899e191991cbff9b9f948cfa12ff9
Pedro Rodrigues <ukpedropt@hotmail.com>
2023-11-02 19:35:17 +0000

    Implemented appending functionality for multiple file renderings

--- app.py
+++ app.py
@@ -1,37 +1,33 @@
-from werkzeug.utils import secure_filename
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template_string
+from flask import Flask, request, send_from_directory, render_template, jsonify  # Import jsonify
 import os
-from docx2pdf import convert
-from ppt2pdf import convert_ppt_to_pdf
+import subprocess
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static/uploads'  # specify your upload folder
 
 @app.route('/')
 def home():
-    return "Hello, Loadify!"
+    return render_template("upload.html")  # Serve the upload.html template on the home page
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'jpg', 'pdf', 'docx', 'ppt'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
-            if filename.rsplit('.', 1)[1].lower() == 'docx':
-                convert(file_path)
-                filename = filename.rsplit('.', 1)[0] + '.pdf'
-            elif filename.rsplit('.', 1)[1].lower() == 'ppt':
-                convert_ppt_to_pdf(file_path)
-                filename = filename.rsplit('.', 1)[0] + '.pdf'
-            return redirect(url_for('uploaded_file', filename=filename))
-    return render_template_string(open("templates/upload.html").read())
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        if filename.rsplit('.', 1)[1].lower() in ['docx', 'ppt']:
+            pdf_filename = filename.rsplit('.', 1)[0] + '.pdf'
+            pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
+            subprocess.run(['unoconv', '-f', 'pdf', '-o', pdf_path, file_path])
+            filename = pdf_filename  # Update filename to the PDF version
+        return jsonify({'filename': filename})  # Return the filename as a JSON response
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
