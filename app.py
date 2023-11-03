6c7d4c9742fa3f9ffbc06dbddad3be6ced796132
Pedro Rodrigues <ukpedropt@hotmail.com>
2023-11-03 18:57:39 +0000

    Updated app.py and register.html to add registration functionality

--- app.py
+++ app.py
@@ -1,32 +1,44 @@
-from flask import Flask, request, send_from_directory, render_template, jsonify, redirect, url_for
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
-import os
-import subprocess
+from flask import Flask, request, render_template, redirect, url_for, jsonify, send_from_directory, flash
 from werkzeug.utils import secure_filename
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from models import db, User
+import os
+import subprocess  # Import subprocess to run unoconv
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'  # Change this to a random secret key
-app.config['UPLOAD_FOLDER'] = 'static/uploads'  # specify your upload folder
+app.secret_key = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/ukped/OneDrive - RTC Education Ltd/Desktop/loadify/instance/users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = 'static/uploads'  # Make sure to add this line
 
+db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-class User(UserMixin):
-    def __init__(self, id):
-        self.id = id
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User(user_id)
+    return User.query.get(int(user_id))
+
+
+@app.route('/home')  # Renamed from '/' to '/home'
+def home():
+    return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user_id = request.form['user_id']
-        user = User(user_id)
-        login_user(user)
-        return redirect(url_for('home'))
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            return "Invalid credentials", 401
     return render_template('login.html')
+
 
 @app.route('/logout')
 @login_required
@@ -34,15 +46,45 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/')
-@login_required
-def home():
-    return render_template("upload.html")  # Serve the upload.html template on the home page
 
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'jpg', 'png', 'pdf', 'docx', 'ppt'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Validate user input
+        if not username or not password or not confirm_password:
+            flash('All fields are required.', 'danger')
+            return render_template('register.html')
+        if password != confirm_password:
+            flash('Passwords do not match.', 'danger')
+            return render_template('register.html')
+
+        # Check if username already exists
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists.', 'danger')
+            return render_template('register.html')
+
+        # Create new user
+        new_user = User(username=username)
+        new_user.set_password(password)  # Hashes the password
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful. Please log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
 
 @app.route('/upload', methods=['POST'])
 @login_required
@@ -57,12 +99,17 @@ def upload():
             pdf_path = os.path.join(app.config['UPLOAD_FOLDER'], pdf_filename)
             subprocess.run(['unoconv', '-f', 'pdf', '-o', pdf_path, file_path])
             filename = pdf_filename  # Update filename to the PDF version
-        return jsonify({'filename': filename})  # Return the filename as a JSON response
+        # Return the filename as a JSON response
+        return jsonify({'filename': filename})
+
 
 @app.route('/uploads/<filename>')
 @login_required
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables
     app.run(debug=True)
